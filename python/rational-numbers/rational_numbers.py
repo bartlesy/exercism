5384e19a1ac8b457691fa721cd720f53b6db8bd6
Stanley Bartlett <stanbartlett@gmail.com>
2018-07-27 16:51:44 -0400

    scala

---
+++ python/rational-numbers/rational_numbers.py
@@ -0,0 +1,54 @@
+from __future__ import division
+from math import gcd
+
+
+class Rational(object):
+    def __init__(self, numer, denom):
+        if not denom:
+            raise DivideByZero("Can't divide by zero")
+
+        gcd_res = gcd(int(numer), int(denom))
+        self.numer = numer / gcd_res
+        self.denom = denom / gcd_res
+
+        if self.numer < 0 and self.denom < 0:
+            self.numer = abs(self.numer)
+            self.denom = abs(self.denom)
+        elif self.denom < 0:
+            self.numer = -self.numer
+            self.denom = -self.denom
+
+    def __eq__(self, other):
+        return self.numer == other.numer and self.denom == other.denom
+
+    def __repr__(self):
+        return '{}/{}'.format(self.numer, self.denom)
+
+    def __add__(self, other):
+        numer = self.numer * other.denom + self.denom * other.numer
+        denom = self.denom * other.denom
+        return Rational(numer, denom)
+
+    def __sub__(self, other):
+        numer = self.numer * other.denom - self.denom * other.numer
+        denom = self.denom * other.denom
+        return Rational(numer, denom)
+
+    def __mul__(self, other):
+        numer = self.numer * other.numer
+        denom = self.denom * other.denom
+        return Rational(numer, denom)
+
+    def __truediv__(self, other):
+        numer = self.numer * other.denom
+        denom = self.denom * other.numer
+        return Rational(numer, denom)
+
+    def __abs__(self):
+        return Rational(abs(self.numer), abs(self.denom))
+
+    def __pow__(self, power):
+        return Rational(self.numer ** power, self.denom ** power)
+
+    def __rpow__(self, base):
+        return base ** (self.numer / self.denom)
