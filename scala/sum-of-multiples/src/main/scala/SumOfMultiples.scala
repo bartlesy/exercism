c78965f10b2cc1ee88c41a76d52230c8488c786a
Stanley Bartlett <stanbartlett@gmail.com>
2018-07-27 16:54:09 -0400

    just remember the until keyword

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,6 +1,6 @@
 object SumOfMultiples {
   def sum(factors: Set[Int], limit: Int): Int = {
-    (0 to limit - 1).filter(x => x % 3 == 0 | x % 5 == 0).reduce(_ + _)
+    (0 until limit).filter(x => x % 3 == 0 | x % 5 == 0).reduce(_ + _)
   }
 }
 
