b6775493947d1f8fe603c23624cc4c24e6812c31
Stanley Bartlett <stanbartlett@gmail.com>
2018-07-28 07:32:35 -0400

    more idiomatic scala

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,6 +1,10 @@
 object SumOfMultiples {
   def sum(factors: Set[Int], limit: Int): Int = {
-    (0 until limit).filter(x => factors.map(y => x % y).contains(0)).sum
+    (0 until limit).filter(x => factors.exists(y => x % y == 0)).sum
+  }
+
+  def sum22(factors: Set[Int], limit: Int) = {
+    factors.flatMap(factor => factor until limit by factor).sum
   }
 }
 
