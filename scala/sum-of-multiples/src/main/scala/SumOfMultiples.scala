3daea0bf21d2093872694b9fecd9340c04b91a80
Stanley Bartlett <stanbartlett@gmail.com>
2018-07-28 07:22:28 -0400

    updating sum of multiples

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,10 +1,6 @@
 object SumOfMultiples {
   def sum(factors: Set[Int], limit: Int): Int = {
-    if (factors.size == 0) return 0
-
-    val outList = (0 until limit).filter(x => factors.map(y => x % y).min == 0)
-
-    if (outList.length > 0) outList.reduce(_ + _) else 0
+    (0 until limit).filter(x => factors.map(y => x % y).contains(0)).sum
   }
 }
 
