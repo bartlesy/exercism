2ff3d0e45b0127a63fbbe53dc55f7450179bd23d
Stanley Bartlett <stanbartlett@gmail.com>
2018-07-27 22:26:00 -0400

    fixed the beer song

--- scala/beer-song/src/main/scala/BeerSong.scala
+++ scala/beer-song/src/main/scala/BeerSong.scala
@@ -9,27 +9,27 @@ object BeerSong {
 
   def get_lines(cnt: Int): String = {
     val next_cnt = cnt - 1
-    var cur_str = new StringBuilder
+    var curStr = new StringBuilder
     if (cnt > 0) {
-      cur_str ++=  s"$cnt bottles of beer on the wall, $cnt bottles of beer.\nTake one down and pass it around, "
+      curStr ++=  s"$cnt bottle${if (cnt > 1) "s" else ""} of beer on the wall, $cnt bottle${if (cnt > 1) "s" else ""} of beer.\n"
       if (next_cnt > 0) {
-        cur_str ++= s"${cnt - 1} bottles of beer on the wall\n"
+        curStr ++= s"Take one down and pass it around, ${next_cnt} bottle${if (next_cnt > 1) "s" else ""} of beer on the wall.\n"
       } else {
-        cur_str ++= s"no more bottes of beer on the wall.\n"
+        curStr ++= s"Take it down and pass it around, no more bottles of beer on the wall.\n"
       }
     } else {
-      cur_str ++= "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+      curStr ++= "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     }
-    cur_str.toString
+    curStr.toString
   }
 
   def recite(n: Int, k: Int): String = {
     val iters = (n to 0 by -1).toList ::: (99 until n by -1).toList
-    var out_arr = new ListBuffer[String]()
+    var outArr = new ListBuffer[String]()
     for (cnt <- cycle(iters) take k) {
-      out_arr += get_lines(cnt)
+      outArr += get_lines(cnt)
     }
-    out_arr.mkString("\n")
+    outArr.mkString("\n")
   }
 }
 
