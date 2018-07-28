319c51342a41afa9ee72929e94f9faf73efcdb2c
Stanley Bartlett <stanbartlett@gmail.com>
2018-07-27 22:06:03 -0400

    o dang didnt realize i had to edit the test files

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,6 +1,10 @@
 object SumOfMultiples {
   def sum(factors: Set[Int], limit: Int): Int = {
-    (0 until limit).filter(x => x % 3 == 0 | x % 5 == 0).reduce(_ + _)
+    if (factors.size == 0) return 0
+
+    val outList = (0 until limit).filter(x => factors.map(y => x % y).min == 0)
+
+    if (outList.length > 0) outList.reduce(_ + _) else 0
   }
 }
 
